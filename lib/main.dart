2d46f8ed026a7e8c8027552d714b0ba9d89a3b69
hannanmiyaji <hannanmiyaji729@gmail.com>
2024-10-08 17:41:17 +0600

    AppBar Radius,ElevatedButton,TextButton,IconButton,OutlinedButton,FloationgActionButton,Border & BorderSide Colors,

--- lib/main.dart
+++ lib/main.dart
@@ -24,27 +24,69 @@ class HomeActivity extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Scrollbar Test"),
+        title: Text("MyApp Bar"),
+        backgroundColor: Colors.green,
+        flexibleSpace: ClipRRect(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(80), topRight: Radius.circular(80)),
+        ),
       ),
-      body: Scrollbar(
-        trackVisibility: true,
-        thickness: 10,
-        interactive: true,
-        radius: const Radius.circular(20),
-        child: ListView.builder(
-            itemCount: 20,
-            itemBuilder: (context, index) {
-              return Container(
-                alignment: Alignment.center,
-                margin: const EdgeInsets.all(8),
-                height: 150,
-                color: Colors.blue.shade100,
-                child: Text(
-                  'Item$index',
-                  style: const TextStyle(fontSize: 22),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
                 ),
-              );
-            }),
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 10),
+                shadowColor: Colors.yellow,
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  side: BorderSide(color: Colors.black, width: 3),
+                ),
+                minimumSize: Size(150, 50),
+                maximumSize: Size(350, 350),
+              ),
+              onPressed: () {
+                print("Tap on elevatedButton");
+              },
+              child: Text("Tap"),
+            ),
+            SizedBox(height: 24),
+            TextButton(
+                style: TextButton.styleFrom(),
+                onPressed: () {
+                  print('Tapped text button');
+                },
+                child: Text("Tap here")),
+            SizedBox(height: 24),
+            IconButton(
+                style: TextButton.styleFrom(),
+                onPressed: () {
+                  print('Tapped text button');
+                },
+                icon: Icon(Icons.home)),
+            SizedBox(height: 24),
+            OutlinedButton(
+                style: TextButton.styleFrom(),
+                onPressed: () {
+                  print('Tapped text button');
+                },
+                child: Text("Click here")),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.account_balance),
       ),
     );
   }
